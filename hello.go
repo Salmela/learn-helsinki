7f94e6de29570840c2071733e514a1cc955b23fd
Aleksi Salmela <aleksi@salmela.dev>
2025-01-11 11:27:56 +0200

    Add bound checking for args

--- hello.go
+++ hello.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
-    fmt.Println(generateTitle(os.Args[1]))
+    if (len(os.Args) > 1) {
+        fmt.Println(generateTitle(os.Args[1]))
+    } else {
+        fmt.Printf("USAGE: %s NAME\n", os.Args[0])
+    }
 }
 
