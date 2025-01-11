930421de67c582a9b5ad4e6e7d111caa5df7a0ba
Aleksi Salmela <aleksi@salmela.dev>
2025-01-11 11:44:04 +0200

    Learning errors and logs

--- another.go
+++ another.go
@@ -2,10 +2,14 @@ package main
 
 import (
     "fmt"
+    "errors"
 )
 
 
-func generateTitle(name string) string {
+func generateTitle(name string) (string, error) {
+    if (len(name) < 3) {
+        return "", errors.New("Too short name")
+    }
     message := fmt.Sprintf("Hello, %s! 👋", name)
-    return message
+    return message, nil
 }
--- hello.go
+++ hello.go
@@ -3,11 +3,19 @@ package main
 import (
     "fmt"
     "os"
+    "log"
 )
 
 func main() {
+    log.SetPrefix("LOG: ")
+    log.SetFlags(0)
+
     if (len(os.Args) > 1) {
-        fmt.Println(generateTitle(os.Args[1]))
+        result, err := generateTitle(os.Args[1])
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Println(result)
     } else {
         fmt.Printf("USAGE: %s NAME\n", os.Args[0])
     }
