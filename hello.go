cb882aa7f219f8c8c79dfafc77f7fc3980678a93
Aleksi Salmela <aleksi@salmela.dev>
2025-01-14 00:02:02 +0200

    Try to use connection pool in backend

--- hello.go
+++ hello.go
@@ -3,23 +3,38 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"io/ioutil"
 	"log"
+	"encoding/json"
 	"net/http"
-	"os"
 )
+
+type QuestionCreationRequest struct {
+	Type string
+	Subject string
+	Answer string
+}
+
+func initDatabase(connection *pgxpool.Pool) {
+	_, err := connection.Query(context.Background(), "CREATE TABLE IF NOT EXISTS questions (type varchar(40), subject varchar(256), answer varchar(4096))")
+	if err != nil {
+		log.Panicf("QueryRow failed: %v\n", err)
+	}
+}
 
 func main() {
 	log.SetPrefix("LOG: ")
 	log.SetFlags(0)
 
-	conn, err := pgx.Connect(context.Background(), "postgres://postgres:example@172.17.0.1:5432/asker")
+	// TODO: is there some nice ORM for go
+	connection, err := pgxpool.New(context.Background(), "postgres://postgres:example@172.17.0.1:5432/asker")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
+		log.Panicf("Unable to connect to database: %v\n", err)
 	}
-	defer conn.Close(context.Background())
+	defer connection.Close()
+
+	initDatabase(connection)
 
 	address := ":8082"
 	fmt.Printf("Starting server at port %s\n", address)
@@ -36,8 +51,16 @@ func main() {
 		if request.Method == "POST" {
 			body, err := ioutil.ReadAll(request.Body)
 			if err != nil {
-				log.Fatal(err)
-				return
+				log.Panicf("Reading response body failed: %v\n", err)
+			}
+			var parsedBody QuestionCreationRequest;
+			err2 := json.Unmarshal(body, &parsedBody)
+			if err2 != nil {
+				log.Panicf("Parsing failed: %v\n", err2)
+			}
+			_, err3 := connection.Query(context.Background(), "INSERT INTO questions (type, subject, answer) VALUES ($1, $2, $3)", )
+			if err3 != nil {
+				log.Panicf("QueryRow failed: %v\n", err3)
 			}
 			fmt.Fprintf(response, string(body))
 		} else {
